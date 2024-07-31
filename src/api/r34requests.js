d08b85aac7d934f841ea81fa0e2be60e6e2fb3a2
Lewdside Quest <lewdsidequest@gmail.com>
2024-07-31 13:06:00 -0700

    r34 fixed, version 2.0 finish?

--- src/api/r34requests.js
+++ src/api/r34requests.js
@@ -1,5 +1,4 @@
-import fetch from "node-fetch";
-import convert from "xml-js";
+import Booru from "booru";
 
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
@@ -9,51 +8,28 @@ function getRandomInt(min, max) {
 
 export async function getRandomR34Post(tagString, limit) {
   let chosenUrl = "Error al obtener resultados";
-  const newStr = encodeURIComponent(tagString);
-  const finalTagString = newStr
-    .replace(/%20/g, "+")
-    .replace(/\(/g, "%28")
-    .replace(/\)/g, "%29");
-  //   console.log("finalTagString: " + finalTagString);
-  const testResponse = await fetch(
-    `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=${finalTagString}&limit=${limit}`,
-    {
-      method: "GET",
-      headers: {
-        "User-Agent": "lewdsidequestTests/1.0",
-        "Content-Type": "text/xml",
-      },
-    }
-  );
-  const xmlfix = await testResponse.text();
-  const data = convert.xml2js(
-    xmlfix
-      .replace(/[\n\r]/g, "\\n")
-      .replace(/&/g, "&amp;")
-      .replace(/-/g, "&#45;"),
-    {
-      compact: false,
-      spaces: 4,
-    }
-  );
-  // console.dir(data, { depth: 3 });
-  const posts = data.elements[0].elements;
+  let posts = await Booru.search("api.rule34.xxx", [tagString], {
+    limit: limit,
+  });
+  // for (let post of posts) console.log(post.fileUrl);
+
   // console.dir(posts, { depth: 3 });
+
   try {
     const maxPosts = posts.length - 1;
-    const chosenPost = posts[getRandomInt(0, maxPosts)].attributes;
+    const chosenPost = posts[getRandomInt(0, maxPosts)];
     if (chosenPost) {
       if (
         chosenPost.tags.includes("animated") &&
-        !chosenPost.sample_url.includes(".gif")
+        !chosenPost.sampleUrl.includes(".gif")
       ) {
         //imprime post animado con file url
-        chosenUrl = chosenPost.file_url;
+        chosenUrl = chosenPost.fileUrl;
       }
       //si el post no esta shadow baneado imprime el sample url
-      else if (chosenPost.file_url) {
+      else if (chosenPost.fileUrl) {
         //imprime post no animado o GIF con sample url
-        chosenUrl = chosenPost.sample_url;
+        chosenUrl = chosenPost.sampleUrl;
       } else {
         chosenUrl = "El resultado aleatorio contiene sample url no valido";
         return chosenUrl;
@@ -65,11 +41,3 @@ export async function getRandomR34Post(tagString, limit) {
     return chosenUrl;
   }
 }
-const r34blacklist = ` -cuntboy -gore -feces -scat -vore -fart -fart_fetish -diaper -andromorph -maleherm -gender_transformation -macro -hyper_belly -pregnant -humiliation -degradation -food -rape -feral -young -loli -shota -toddler -bestiality -zoophilia`;
-console.log(
-  await getRandomR34Post(
-    "( raven_(dc) ~ gwen_tennyson ~ lord_dominator ~ loona_(helluva_boss) ~ rebecca_(edgerunners) ~ azula ~ nadia_fortune ~ elastigirl ~ matoi_ryuuko ~ catra ~ rose_the_cat ) female -rating:safe -flat_chested -futanari -small_breasts score:>=100" +
-      r34blacklist,
-    2
-  )
-);
