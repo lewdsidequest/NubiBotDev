258e22a6aeaf2fc485b5d13cc8fec26991bac27c
Lewdside Quest <lewdsidequest@gmail.com>
2024-07-31 18:50:58 -0700

    xbooru test

--- src/api/r34requests.js
+++ src/api/r34requests.js
@@ -14,33 +14,13 @@ function getRandomInt(min, max) {
 export async function getRandomR34Post(tagString, limit) {
   let chosenUrl = "Error al obtener resultados";
   try {
-    const myBooru = Booru.forSite("api.rule34.xxx", {
-      authUser: process.env.R34UN,
-      password: process.env.R34PASS,
-    });
+    const myBooru = Booru.forSite("xbooru.com");
 
     let posts = await myBooru.search(tagString, { limit: limit });
-
     // console.dir(posts, { depth: 3 });
     const maxPosts = posts.length - 1;
     const chosenPost = posts[getRandomInt(0, maxPosts)];
-    if (chosenPost) {
-      if (
-        chosenPost.tags.includes("animated") &&
-        !chosenPost.sampleUrl.includes(".gif")
-      ) {
-        //imprime post animado con file url
-        chosenUrl = chosenPost.fileUrl;
-      }
-      //si el post no esta shadow baneado imprime el sample url
-      else if (chosenPost.fileUrl) {
-        //imprime post no animado o GIF con sample url
-        chosenUrl = chosenPost.sampleUrl;
-      } else {
-        chosenUrl = "El resultado aleatorio contiene sample url no valido";
-        return chosenUrl;
-      }
-    }
+    chosenUrl = chosenPost.fileUrl;
     return chosenUrl;
   } catch (err) {
     console.log("Error al conectar con la API de r34\n" + err.message);
